878ed58d386376a3d0be40b70f771ba241b4d360
Ianny Mamedes <105374233+iannymamedes@users.noreply.github.com>
2023-09-17 17:47:39 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Primeiro from './basicos/Primeiro';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>App React</h1>
+            <Primeiro />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/basicos/Primeiro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Primeiro() {
+    return (
+        <div>
+            <h1>Usando react para a disciplina de FrontEnd-lll</h1>
+            <a href='#'>https://github.com/iannymamedes</a>
+            <p>Aluna: Ianny R. Mamedes da Silva</p>
+        </div>
+
+    );
+}
+
+export default Primeiro;
